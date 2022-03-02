897b369fe9257165cd7f440befdd855ecbfe02d6
mikemorter <mikemorter@gmail.com>
2022-03-02 14:59:25 -0500

    heroku addition

---
+++ build/precache-manifest.db466b1b0a10d79db768317cb66b330e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b537d04776675e32ac67ce70c0867b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be29eae2d1837b49652b",
+    "url": "/static/js/2.de1d5b3d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.de1d5b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f752c90a60f66b83103f",
+    "url": "/static/js/main.afa69a8f.chunk.js"
+  },
+  {
+    "revision": "08d876ca5bd05d3ea4f0",
+    "url": "/static/js/runtime-main.b2a68d07.js"
+  }
+]);
